78c6cdf56f3b2c2df0a47ad6921b63443ea15ebe
Miza-web <miza.four@gmail.com>
2023-07-06 22:09:11 +0100

    created header component

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,11 @@
 import {defineComponent} from "vue";
 import navbar from "@/components/navbar.vue";
 import footer_main from "@/components/footer_main.vue";
+import header_main from "@/components/Header_main.vue";
+import landscape from "@/components/landscape.vue";
 
 export default defineComponent({
-  data() {
-    return {
-      show: true,
-      hover: false
-    }
-  },
-  components: {navbar, footer_main}
+  components: {navbar, footer_main, header_main, landscape}
 },)
 
 </script>
@@ -18,85 +14,14 @@ export default defineComponent({
 <template>
   <header>
   <navbar />
-    <div>
-    <img class="header" alt="edinburgh" src="@/assets/img/edinburgh-header.jpg"/>
-      <transition appear>
-      <div class="header-text" v-if="show">Discover Edinburgh</div>
-      </transition>
-    </div>
+  <header_main />
   </header>
-
   <main>
-    <div class="col landscape">
-      <h1>Edinburgh's Landscape is one of a kind</h1>
-      <h3>Being a coastal city, Edinburgh has access to the sea, while also featuring hills, beautiful architecture and more!</h3>
-    </div>
-  <div class="container">
-    <div class="row">
-      <div class="col itemsContainer" @mouseover="hover = true" @mouseleave="hover = false">
-        <div class="icon_div" v-if="hover"><img class="fs_icon" src="./components/icons/fullscreen_icon.png"/></div>
-        <img class="landscape-thumbnails" alt="Arthur's seat in Edinburgh" src="@/assets/img/arthurs-seat.jpg">
-      </div>
-      <div class="col itemsContainer" @mouseover="hover = true" @mouseleave="hover = false">
-        <div class="icon_div" v-if="hover"><img class="fs_icon" src="./components/icons/fullscreen_icon.png"/></div>
-        <img class="landscape-thumbnails" alt="Arthur's seat in Edinburgh" src="@/assets/img/arthurs-seat.jpg">
-      </div>
-      <div class="col itemsContainer" @mouseover="hover = true" @mouseleave="hover = false">
-        <div class="icon_div" v-if="hover"><img class="fs_icon" src="./components/icons/fullscreen_icon.png"/></div>
-        <img class="landscape-thumbnails" alt="Arthur's seat in Edinburgh" src="@/assets/img/arthurs-seat.jpg">
-      </div>
-    </div>
-    </div>
+    <landscape />
     <footer_main />
   </main>
 </template>
 
 <style scoped>
-.header {
-  max-width: 100%;
-}
-.header-text {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  font-size: 110px;
-  color: white;
-  text-shadow: 3px 3px black;
-}
-.landscape {
-  margin-top: 20px;
-  text-align: center;
-}
-.landscape-thumbnails {
-  width: 330px;
-}
-.landscape-thumbnails:hover {
-  opacity: 0.5;
-  border: 1px solid black;
-}
-.itemsContainer {
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  justify-content: center;
-}
-.itemsContainer:hover .icon_div{display:block}
-.icon_div {
-  position: absolute;
-  display: none;
-  width: 50px;
-}
-.fs_icon {
-  width: 75px;
-}
-.v-enter-active,
-.v-leave-active {
-  transition: opacity 0.5s ease-in;
-}
 
-.v-enter-from,
-.v-leave-to {
-  opacity: 0;
-}
 </style>
---
+++ src/components/Header_main.vue
@@ -0,0 +1,47 @@
+<script>
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  data(){
+    return {
+      show: true
+    }
+  },
+  name: "header_main",
+});
+</script>
+<template>
+  <div>
+    <img
+      class="header"
+      alt="edinburgh"
+      src="@/assets/img/edinburgh-header.jpg"
+    />
+    <transition appear>
+      <div class="header-text" v-if="show">Discover Edinburgh</div>
+    </transition>
+  </div>
+</template>
+<style scoped>
+.header {
+  max-width: 100%;
+}
+.header-text {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 110px;
+  color: white;
+  text-shadow: 3px 3px black;
+}
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease-in;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
